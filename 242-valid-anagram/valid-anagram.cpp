d75298f2d4aba6d1117e191551475cf0d44859e7
aayushrajawat <aayushrajawat2006@gmail.com>
2026-09-10 12:45:25 +0530

    Time: 1 ms (53.88%) | Memory: 9.8 MB (25.64%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.length()!=t.length()) return false;
+
+        unordered_map<char,int> m;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t[i];
+            if(m.find(ch)!=m.end()){
+                m[ch]--;
+                if(m[ch]==0) m.erase(ch);
+            }
+        }
+        if(m.size()>0) return false;
+        return true;
+    }
+};
